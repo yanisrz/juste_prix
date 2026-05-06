1904f906c149d2c6eb58040c91181b29d7252d6b
yanis R-Z <yanis.rz@gmail.com>
2026-05-06 20:46:09 +0200

    add level

--- jeu.py
+++ jeu.py
@@ -1,8 +1,23 @@
 import random
 import time 
-prix = random.randint(1, 50)
+
+niveau = input("     Choisis ton niveau de difficulté (facile = 1, difficile = 2): ")
+
+if niveau == "1":
+    prix = random.randint(1, 50)
+    grand_prix = 50
+elif niveau == "2":
+    prix = random.randint(1, 100)
+    grand_prix = 100
+else:
+    print("     Niveau de difficulté pas valide, le jeu se lance en mode facile par defaut")
+    prix = random.randint(1,5) 
+    grand_prix = 50
+
+
+
 debut_chrono = time.time()
-devine="     Devines le prix entre 1 et 50: "
+devine="     Devines le prix entre 1 et " + str(grand_prix) + ": "
 compteur: int = 0
 print( devine )
 while devine != prix:
