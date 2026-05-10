057b372bfb63d3be07696dea34aa52770ca8f035
yanis R-Z <yanis.rz@gmail.com>
2026-05-10 15:41:50 +0200

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -1,37 +1,39 @@
 import random
 import time 
-#differentes librairies utilisées
-#on demande a l'utilisateur de choisir le niveau
+# Différentes librairies natives utilisées
+# On demande à l'utilisateur de choisir le niveau
 niveau = input("     Choisis ton niveau de difficulté (facile = 1, difficile = 2) : ")
-# le prix est generer en fonction du iveau choisi
+# Le prix est généré en fonction du niveau choisi
+grand_prix = 50 # Valeur par défaut du grand prix 
 if niveau == "1":
-    prix = random.randint(1, 50)
-    grand_prix = 50
+    print("     Tu as choisi le niveau facile")
+
 elif niveau == "2":
-    prix = random.randint(1, 500)
-    grand_prix = 100
+    grand_prix = 500
+    print("     Tu as choisi le niveau difficile ")
+    
 else:
-    print("     Niveau de difficulté pas valide, le jeu se lance en mode facile par défaut")
-    prix = random.randint(1,5) 
-    grand_prix = 50
+    print("    😡  Niveau de difficulté pas valide, le jeu se lance en mode facile par défaut ")
+prix = random.randint(1,grand_prix)
+    
 
 debut_chrono = time.time()
-#on ajoute le grand prix dans le texte
+# On ajoute le grand prix dans le texte
 devine = (f"     Devine le prix entre 1 et {grand_prix} : ")
 compteur: int = 0
 print( devine) 
-#on debute une boucle tant que "prix" est different de "devine" zu debut elle l'est car ce n'est meme pas un nombre entier 
+# On débute une boucle tant que "prix" est différent de "devine". Au début elle l'est car ce n'est même pas un nombre entier 
 while devine != prix:
     compteur += 1
-    # on demande à l'utilisateur de deviner
+    # On demande à l'utilisateur de deviner
     devine = input("     \n     ")
-    #on verifie que "devine" est un nombre entier
+    # On vérifie que "devine" est un nombre entier
     if devine.isdigit() == False:
         print("     !!Ce n'est pas un nombre, essaie encore !!😡\n")
-        #on remonte au haut de la boucle
+        # On remonte au haut de la boucle
         continue
     
-    #on cnvertie " devine" en entier pour povoir le comparer
+    # On convertit "devine" en entier pour pouvoir le comparer
     devine = int(devine)
     if devine < prix:
         print("     C'est trop petit !!")
@@ -42,7 +44,7 @@ while devine != prix:
 
 fin_chrono = time.time()
 chrono = int(fin_chrono-debut_chrono)
-#on convertie les secondes en minutes et secondes
+# On convertit les secondes en minutes et secondes
 minutes, secondes = divmod(chrono, 60)
 
 if minutes == 0:
